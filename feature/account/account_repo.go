88d304ca39650e633b5dc6a0d13a3acd751b63f4
koanba <koanba@koanbas-MacBook-Pro-3.local>
2023-09-06 09:31:18 +0700

    add cucumber

--- feature/account/account_repo.go
+++ feature/account/account_repo.go
@@ -12,7 +12,7 @@ func insert(account Account) (pgconn.CommandTag, error) {
 		id, _ := uuid.NewUUID()
 		result, err := db.Pg.Exec(
 			context.Background(),
-			`INSERT INTO account_test (id, username, email, password) values ($1, $2, $3, $4)`,
+			`INSERT INTO account (id, username, email, password) values ($1, $2, $3, $4)`,
 			id.String(), account.Username, account.Email, account.Password)
 		return result, err
 	})
