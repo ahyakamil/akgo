ab8141441d82a6ef84f1ed51747e89796dfda6bf
koanba <koanba@koanbas-MacBook-Pro-4.local>
2024-02-08 11:12:51 +0700

    improve pageable

--- config/pageable.go
+++ config/pageable.go
@@ -1,13 +1,34 @@
 package config
 
+import (
+	"net/url"
+	"strconv"
+)
+
 type Pageable struct {
 	Page int `json:"page"`
 	Size int `json:"size"`
 }
 
-func NewPageable() Pageable {
-	return Pageable{
+func NewPageable(url *url.URL) Pageable {
+	result := Pageable{
 		Page: 0,
 		Size: 10,
 	}
+
+	if url.Query().Get("page") != "" {
+		page, err := strconv.Atoi(url.Query().Get("page"))
+		if err == nil {
+			result.Page = page
+		}
+	}
+
+	if url.Query().Get("size") != "" {
+		size, err := strconv.Atoi(url.Query().Get("size"))
+		if err == nil {
+			result.Size = size
+		}
+	}
+
+	return result
 }
